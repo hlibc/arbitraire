e59f8768f5b71c0404fadda0255dac242e444ee2
graff <cm0graff@gmail.com>
2018-11-13 18:08:15 -0600

    documentation for long-multiplication

--- src/long-multiplication.c
+++ src/long-multiplication.c
@@ -1,15 +1,23 @@
 #include "internal.h"
+
 /*
 	A "partial carry" variation on the traditional "school book" long
 	multiplication algorithm. Arrays are accessed via n-1 to allow unsigned
-	types to be used therefore obtaining the maximum possible indexable 
-	variation.
+	types to be used therefore allowing the maximum possible datum. Here,
+	a size_t is used, though future developers may want to use a uint64_t
+	or other fived-width datum.
 
 	I've devised what appears to be a somewhat novel method for making the
 	partial carry long multiplication algorithm zelf zeroing. Many of these
 	algorithms depend on the arrays being zeroed out first, but this
-	extra step alleviates the need for this. 
+	extra step alleviates the need for this. In arbitraire this is used
+	for our implementation of Knuth's algorithm D (division) and allows
+	arn_mul_core to stand alone.
 
+	Multiplication does not require temporary stores of the variable and
+	can be calculated without prior knowledge of the magnitude of the
+	inputs. However, here we make copies of the data to allow function
+	usages such as f(a, a, a) as opposed to needing f(a, b, c).
 */
 
 size_t arb_mul_core(ARBT *a, size_t alen, ARBT *b, size_t blen, ARBT *c, int base)
@@ -29,8 +37,7 @@ size_t arb_mul_core(ARBT *a, size_t alen, ARBT *b, size_t blen, ARBT *c, int bas
 			carry = prod / base;
 			c[k-1] = (prod % base);
 		}
-		/* self zeroing multiplication is used in algorithm D (division)
-		and allows arb_mul_core to stand alone. */
+		/* self zeroing */
 		if (k != last) {
 			++ret;
 			c[k-1] = 0;
@@ -42,6 +49,9 @@ size_t arb_mul_core(ARBT *a, size_t alen, ARBT *b, size_t blen, ARBT *c, int bas
 
 fxdpnt *arb_mul(fxdpnt *a, fxdpnt *b, fxdpnt *c, int base, size_t scale)
 {
+	/* a2 and b2 are likely superfluous, and are slated for removal.
+		However, this may require a special method for scaling.
+	*/
 	fxdpnt *a2 = arb_expand(NULL, MAX(scale, a->len));
 	fxdpnt *b2 = arb_expand(NULL, MAX(scale, b->len));
 	fxdpnt *c2 = arb_expand(NULL, a2->len + b2->len);
@@ -57,4 +67,9 @@ fxdpnt *arb_mul(fxdpnt *a, fxdpnt *b, fxdpnt *c, int base, size_t scale)
 	if (c)
 		arb_free(c);
 	return c2;
+}
+
+void arb_mul2(fxdpnt *a, fxdpnt *b, fxdpnt **c, int base, size_t scale)
+{
+	*c = arb_mul(a, b, *c, base, scale);
 }
