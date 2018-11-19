f28625b08a2fe6bacb17345aa71852521839cb3a
graff <cm0graff@gmail.com>
2018-11-19 13:33:21 -0600

    start the karatsuba algorithm

---
+++ src/karatsuba.c
@@ -0,0 +1,16 @@
+#include "internal.h"
+
+fxdpnt *karatsuba(fxdpnt *a, fxdpnt *b, fxdpnt *c, int base, size_t scale)
+{
+	/* divide the number into halves */
+	size_t halfa = a->len / 2;
+	size_t halfb = b->len / 2;
+	/* long half */
+	
+
+	/* middle halves */
+
+	/* short halves */
+	
+	return c;
+}
