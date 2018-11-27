c6d0b7107fa2557c3d802bc5c683aec16e686fc5
graff <cm0graff@gmail.com>
2018-11-27 03:09:58 -0600

    clean up sqrt

--- src/newtonian-sqrt.c
+++ src/newtonian-sqrt.c
@@ -1,15 +1,9 @@
 #include "internal.h"
 
 /*
-	Newton's swuare root is a typical algorithm which is more or less
+	Newton's square root is a typical algorithm which is more or less
 	identical to the Babylonian Method. We deploy a variation which tests
-	the scales of the two guesses and allows the formula to break out of
-	the loop, ergo avoiding oscillation and other pitfalls.
-
-	This method always produces correct answers as it is scaled past the
-	needed precision and it never siezes up in an infinite loop as it has
-	a mechanism for breaking out of the loop based on the scale of the
-	converging "guesses".
+	the scales of the two guesses.
 */
 
 fxdpnt *nsqrt(fxdpnt *a, int base, size_t scale)
@@ -34,7 +28,7 @@ fxdpnt *nsqrt(fxdpnt *a, int base, size_t scale)
 		g1 = arb_mul(g1, p5, g1, base, 0);
 		g = arb_exp(g, g1, g, base, 0);
 	}
-	//size_t i = 0;
+
 	for(s1 = MAX(rr(a), scale);;)
 	{
 		arb_copy(g1, g);
@@ -48,8 +42,6 @@ fxdpnt *nsqrt(fxdpnt *a, int base, size_t scale)
 			else
 				break;
 		}
-		//printf("iterations to factor %zu\n", i);
-		//++i;
 	}
 	a = arb_div(g, one, a, base, s1);
 	arb_free(g);
