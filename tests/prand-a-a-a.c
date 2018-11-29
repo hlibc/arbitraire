bed5ea98914686e6928165c9ec999c1e86c302a0
graff <cm0graff@gmail.com>
2018-11-29 08:23:52 -0600

    shorten code in prand-a-a-a

--- tests/prand-a-a-a.c
+++ tests/prand-a-a-a.c
@@ -5,16 +5,20 @@
 
 int main(int argc, char *argv[])
 {
-
-	if ( argc < 2)
+	if (argc < 2)
 	{
 		printf("requires a test type, like 'div' or 'sub'\n");
 		return 1;
 	}
+
 	FILE *fp = fopen("testing.bc", "w+");
+	char *string1 = NULL;
+	size_t scale = random() % MAXIMA;
+	fxdpnt *a = NULL;
+
 	if (fp == NULL)
 		return 1;
-	char *string1;
+	
 	if (strcmp(argv[1], "sqrt") == 0)
 		string1 = make_bignum(MAXIMA, 10, 0);
 	else
@@ -23,42 +27,35 @@ int main(int argc, char *argv[])
 	if (string1 == NULL)
 		return 1;
 
-	size_t scale = random() % MAXIMA;
+	a = arb_str2fxdpnt(string1);
+
 	fprintf(fp, "scale=%zu;\n", scale);
 
-	if (strcmp(argv[1], "sqrt") == 0)
+	if (strcmp(argv[1], "sqrt") == 0) {
 		fprintf(fp, "sqrt(%s)\nquit\n", string1);
-
-	if (strcmp(argv[1], "div") == 0)
-		fprintf(fp, "%s / %s\nquit\n", string1, string1);
-	
-	if (strcmp(argv[1], "add") == 0)
-		fprintf(fp, "%s + %s\nquit\n", string1, string1);
-	
-	if (strcmp(argv[1], "sub") == 0)
-		fprintf(fp, "%s - %s\nquit\n", string1, string1);
-	
-	if (strcmp(argv[1], "mul") == 0)
-		fprintf(fp, "%s * %s\nquit\n", string1, string1);
-
-	fxdpnt *a;
-	a = arb_str2fxdpnt(string1);
-	
-	if (strcmp(argv[1], "div") == 0)
-		a = arb_div(a, a, a, 10, scale);
-	
-	if (strcmp(argv[1], "add") == 0)
-		a = arb_add(a, a, a, 10);
-	
-	if (strcmp(argv[1], "sub") == 0)
-		a = arb_sub(a, a, a, 10);
-	
-	if (strcmp(argv[1], "mul") == 0)
-		a = arb_mul(a, a, a, 10, scale);
-	
-	if (strcmp(argv[1], "sqrt") == 0)
 		a = nsqrt(a, 10, scale);
+	}
+
+	if (strcmp(argv[1], "div") == 0) {
+		fprintf(fp, "%s / %s\nquit\n", string1, string1);
+		a = arb_div(a, a, a, 10, scale);
+	}
 	
+	if (strcmp(argv[1], "add") == 0) {
+		fprintf(fp, "%s + %s\nquit\n", string1, string1);
+		a = arb_add(a, a, a, 10);
+	}
+	
+	if (strcmp(argv[1], "sub") == 0) {
+		fprintf(fp, "%s - %s\nquit\n", string1, string1);
+		a = arb_sub(a, a, a, 10);
+	}
+
+	if (strcmp(argv[1], "mul") == 0) {
+		fprintf(fp, "%s * %s\nquit\n", string1, string1);
+		a = arb_mul(a, a, a, 10, scale);
+	}
+
 	arb_print(a);
 	arb_free(a);
 	free(string1);
--- tests/pseudo-random-tests.c
+++ tests/pseudo-random-tests.c
@@ -7,9 +7,11 @@
 int main(int argc, char *argv[])
 {
 	fxdpnt *a, *b, *c;
-	FILE *fp;
-	char *string1, *string2;
-	size_t scale;
+	FILE *fp = NULL;
+	char *string1 = NULL;
+	char *string2 = NULL;
+	size_t scale = NULL;
+	a = b = c = NULL;
 	
 	if ( argc < 2)
 	{
@@ -26,50 +28,48 @@ int main(int argc, char *argv[])
 	
 
 	scale = random() % MAXIMA;
-	fprintf(fp, "scale=%zu;\n", scale);
-	if (strcmp(argv[1], "div") == 0)
-		fprintf(fp, "%s / %s\nquit\n", string1, string2);
 	
-	if (strcmp(argv[1], "newtonian-div") == 0)
-		fprintf(fp, "%s / %s\nquit\n", string1, string2);
 	
-	if (strcmp(argv[1], "add") == 0)
-		fprintf(fp, "%s + %s\nquit\n", string1, string2);
-	
-	if (strcmp(argv[1], "sub") == 0)
-		fprintf(fp, "%s - %s\nquit\n", string1, string2);
-
-	if (strcmp(argv[1], "mul") == 0)
-		fprintf(fp, "%s * %s\nquit\n", string1, string2);
-	
-	if (strcmp(argv[1], "mod") == 0)
-		fprintf(fp, "%s %% %s\nquit\n", string1, string2);
-
-
 	if (!(a = arb_str2fxdpnt(string1)))
 		return 1;
 	if (!(b = arb_str2fxdpnt(string2)))
 		return 1;
 	if (!(c = arb_expand(NULL, MAXIMA*2)))
 		return 1;
-	
-	if (strcmp(argv[1], "div") == 0)
+
+
+	fprintf(fp, "scale=%zu;\n", scale);
+
+
+	if (strcmp(argv[1], "div") == 0) {
+		fprintf(fp, "%s / %s\nquit\n", string1, string2);
 		c = arb_div(a, b, c, 10, scale);
 	
 	if (strcmp(argv[1], "newtonian-div") == 0)
+		fprintf(fp, "%s / %s\nquit\n", string1, string2);
 		c = arb_newtonian_div(a, b, c, 10, scale);
+	}
 	
-	if (strcmp(argv[1], "add") == 0)
+	if (strcmp(argv[1], "add") == 0) {
+		fprintf(fp, "%s + %s\nquit\n", string1, string2);
 		c = arb_add(a, b, c, 10);
+	}
 	
-	if (strcmp(argv[1], "sub") == 0)
+	if (strcmp(argv[1], "sub") == 0) {
+		fprintf(fp, "%s - %s\nquit\n", string1, string2);
 		c = arb_sub(a, b, c, 10);
-	
-	if (strcmp(argv[1], "mul") == 0)
+	}
+
+	if (strcmp(argv[1], "mul") == 0) {
+		fprintf(fp, "%s * %s\nquit\n", string1, string2);
 		c = arb_mul(a, b, c, 10, scale);
+	}
 	
-	if (strcmp(argv[1], "mod") == 0)
+	if (strcmp(argv[1], "mod") == 0) {
+		fprintf(fp, "%s %% %s\nquit\n", string1, string2);
 		c = arb_mod(a, b, c, 10, scale);
+	}
+
 
 	free(string1);
 	free(string2);
