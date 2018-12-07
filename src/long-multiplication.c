67bcdbe8b119bfd4a448f7b864de4227bf6e8468
graff <cm0graff@gmail.com>
2018-12-06 22:32:28 -0600

    long-multiplication -- improve the comments, remove redundant information and old information. remove unused parens. 'ret' was getting initialized to a bogus value and then later correctly reset

--- src/long-multiplication.c
+++ src/long-multiplication.c
@@ -3,21 +3,15 @@
 /*
 	A "partial carry" variation on the traditional "school book" long
 	multiplication algorithm. Arrays are accessed via n-1 to allow unsigned
-	types to be used therefore allowing the maximum possible datum. Here,
-	a size_t is used, though future developers may want to use a uint64_t
-	or other fived-width datum.
+	types to be used therefore allowing the largest possible unsigned data
+	type for indexable arrays.
 
 	I've devised what appears to be a somewhat novel method for making the
 	partial carry long multiplication algorithm zelf zeroing. Many of these
 	algorithms depend on the arrays being zeroed out first, but this
 	extra step alleviates the need for this. In arbitraire this is used
 	for our implementation of Knuth's algorithm D (division) and allows
-	arn_mul_core to stand alone.
-
-	Multiplication does not require temporary stores of the variable and
-	can be calculated without prior knowledge of the magnitude of the
-	inputs. However, here we make copies of the data to allow function
-	usages such as f(a, a, a) as opposed to needing f(a, b, c).
+	arb_mul_core to stand alone.
 */
 
 size_t arb_mul_core(UARBT *a, size_t alen, UARBT *b, size_t blen, UARBT *c, int base)
@@ -27,16 +21,15 @@ size_t arb_mul_core(UARBT *a, size_t alen, UARBT *b, size_t blen, UARBT *c, int 
 	size_t j = 0;
 	size_t k = 0;
 	size_t last = 0;
-	size_t ret = blen;
-	c[k] = 0;
+	size_t ret = 0;
+	c[0] = 0;
 	c[alen+blen-1] = 0;
-	ret = 0;
 	for (i = alen; i > 0 ; i--){
 		last = k;
 		for (j = blen, k = i + j, carry = 0; j > 0 ; j--, k--){
-			prod = (a[i-1]) * (b[j-1]) + (c[k-1]) + carry;
+			prod = a[i-1] * b[j-1] + c[k-1] + carry;
 			carry = prod / base;
-			c[k-1] = (prod % base);
+			c[k-1] = prod % base;
 		}
 		/* self zeroing */
 		if (k != last) { 
@@ -50,24 +43,19 @@ size_t arb_mul_core(UARBT *a, size_t alen, UARBT *b, size_t blen, UARBT *c, int 
 
 fxdpnt *arb_mul(fxdpnt *a, fxdpnt *b, fxdpnt *c, int base, size_t scale)
 { 
+	fxdpnt *c2 = c;
 	if (a == c || b == c) {
-		fxdpnt *c2 = arb_expand(NULL, a->len + b->len);
-		arb_setsign(a, b, c2);
-		arb_mul_core(a->number, a->len, b->number, b->len, c2->number, base);
-		c2->lp = a->lp + b->lp;
-		c2->len = MIN(rr(a) + rr(b), MAX(scale, MAX(rr(a), rr(b)))) + c2->lp;
-		c2 = remove_leading_zeros(c2);
+		c2 = arb_expand(NULL, a->len + b->len);
+	} else
+		c2 = arb_expand(c2, a->len + b->len);
+	arb_setsign(a, b, c2);
+	arb_mul_core(a->number, a->len, b->number, b->len, c2->number, base);
+	c2->lp = a->lp + b->lp;
+	c2->len = MIN(rr(a) + rr(b), MAX(scale, MAX(rr(a), rr(b)))) + c2->lp;
+	c2 = remove_leading_zeros(c2);
+	if (a == c || b == c)
 		arb_free(c);
-		return c2;
-	}
-
-	c = arb_expand(c, a->len + b->len);
-	arb_setsign(a, b, c);
-	arb_mul_core(a->number, a->len, b->number, b->len, c->number, base);
-	c->lp = a->lp + b->lp;
-	c->len = MIN(rr(a) + rr(b), MAX(scale, MAX(rr(a), rr(b)))) + c->lp;
-	c = remove_leading_zeros(c);
-	return c;
+	return c2;
 }
 
 void mul(fxdpnt *a, fxdpnt *b, fxdpnt **c, int base, size_t scale, char *m)
@@ -76,3 +64,4 @@ void mul(fxdpnt *a, fxdpnt *b, fxdpnt **c, int base, size_t scale, char *m)
 	*c = arb_mul(a, b, *c, base, scale);
 	_internal_debug_end;
 }
+
