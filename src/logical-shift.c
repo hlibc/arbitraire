f081aed75574b74fde2c2b43e616b1094c2cf637
graff <cm0graff@gmail.com>
2018-12-08 19:20:39 -0600

    style

--- src/logical-shift.c
+++ src/logical-shift.c
@@ -11,7 +11,7 @@ fxdpnt *arb_leftshift(fxdpnt *a, size_t n, int faux)
 	for (i = n-1;i < a->len-1; ++i, ++j, ++k)
 		a->number[k] = a->number[j];
 
-	while (l > a->len -1 - n)
+	while (l > a->len-1 - n)
 		a->number[l--] = 0;
 
 	return a;
@@ -26,12 +26,11 @@ void rightshift_core(UARBT *a, size_t len, size_t n)
 
 fxdpnt *arb_rightshift(fxdpnt *a, size_t n, int faux)
 {
-	/* logical right shift, turns base 10 "990" into "099" */
 	if (faux == 0) {
 		rightshift_core(a->number, a->len, n);
 	}
 	else {
-		while (n--&& a->len > 0)
+		while (n-- && a->len > 0)
 			a->len--;
 	}
 	return a;
