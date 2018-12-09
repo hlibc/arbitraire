ca839f1e67d7ff27c6e63b20ab06b4a082b0102b
graff <cm0graff@gmail.com>
2018-12-08 19:20:01 -0600

    style

--- src/remove_leading_zeros.c
+++ src/remove_leading_zeros.c
@@ -5,15 +5,13 @@ fxdpnt *remove_leading_zeros(fxdpnt *c)
 	int effect = 0;
 	size_t i = 0;
 	
-	for (i=0;c->number[i] == 0 && (c->lp > 1 || (c->lp > 0 && rr(c)));)
-	{ 
+	for (i=0;c->number[i] == 0 && (c->lp > 1 || (c->lp > 0 && rr(c)));) { 
 		c->lp--;
 		++i;
 		effect = 1;
 	}
 
-	if (effect)
-	{
+	if (effect) {
 		c = arb_leftshift(c, i, 1);
 		c->len -= i;
 	}
