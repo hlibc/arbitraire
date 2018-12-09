495c923237b59f79b1a19b6162b5850982e71299
graff <cm0graff@gmail.com>
2018-12-08 22:11:10 -0600

    use a library to standardize shell printing functions

--- tests/random-wrapper.sh
+++ tests/random-wrapper.sh
@@ -2,41 +2,40 @@
 
 set -e
 
+. ./.config
+
 if [ "$#" -lt 3 ]
-then	printf "Requires a test type, like 'div', 'add', 'sub' or 'mul'\n"
-	printf "Requires a maxima\m"
-	printf "Must specify 'agnostic' or not\n"
-	exit 1
+then	fatal "Usage: ./tests/random-wrapper.sh div|sub maxima agnostic|null"
 fi
 
 USE_VALGRIND="0"
 USE_STRACE="0"
 USE_TIME="0"
-
-[ "$4" = "valgrind" ] && USE_VALGRIND="1"
-[ "$4" = "strace" ] && USE_STRACE="1"
-[ "$4" = "time" ] && USE_TIME="1"
-
+if [ $# = "4" ]
+then	[ "$4" = "valgrind" ] && USE_VALGRIND="1"
+	[ "$4" = "strace" ] && USE_STRACE="1"
+	[ "$4" = "time" ] && USE_TIME="1"
+fi
 
 COUNT="0"
-
-VERSION=$(cat .version)
+VERSION=$(testcat .version)
 
 machinename="$(gcc -dumpmachine)"
 machinename="${VERSION}-${machinename}-tests-passed.txt"
 
-printf "%s\n" "This is a set of PRNG tests to ensure that arbitraire ${VERSION} works" >>"${machinename}"
-printf "%s\n" "properly on the following machine type:" >>"${machinename}"
-printf "\n" >> "${machinename}"
+stdout "This is a set of PRNG tests to ensure that arbitraire ${VERSION} works" >>"${machinename}"
+stdout "properly on the following machine type:" >>"${machinename}"
+stdout >> "${machinename}"
 uname -a >> "${machinename}"
-gcc -dumpmachine >> "${machinename}"
+stdout "${machinename}" >> "${machinename}"
 
-printf "%s\n" "The tests should halt upon detecting an error and the contents" >>"${machinename}"
-printf "%s\n" "of 'testing.bc' can be inspected to reveal the failing test" >>"${machinename}"
-printf "\n\n" >> "${machinename}"
+stdout "The tests should halt upon detecting an error and the contents" >>"${machinename}"
+stdout "of 'testing.bc' can be inspected to reveal the failing test" >>"${machinename}"
+stdout >> "${machinename}"
+stdout >> "${machinename}"
 while [ "$COUNT" -lt 100 ]
 do	COUNT="$((COUNT + 1))"
-	printf "%s\n" "Test number: ${COUNT}" >>"${machinename}"
+	stdout "Test number: ${COUNT}" >>"${machinename}"
 	if [ "$USE_VALGRIND" = "1" ]
 	then	valgrind --leak-check=full ./tests/random-tests "$1" "$2" "$3" >log 2>log3
 	elif [ "$USE_STRACE" = "1" ]
@@ -46,16 +45,16 @@ do	COUNT="$((COUNT + 1))"
 	else	./tests/random-tests "$1" "$2" "$3" >log 2>log3
 	fi
 	bc -l testing.bc > log2
+	testcat testing.bc >> "${machinename}"
 	if diff log log2
-	then	printf "%s\n" "Giant pseudo-random number test passed."
-		printf "%s\n" "Giant pseudo-random number test passed." >> "${machinename}"
-	else	printf "%s\n"  "Test failed! look inside of '${machinename}' to reveal the failing numbers!" 
-		printf "%s\n"  "Test failed! look inside of '${machinename}' to reveal the failing numbers!" >> "${machinename}"
-		exit 1
+	then	stdout "Giant pseudo-random number test passed."
+		stdout "Giant pseudo-random number test passed." >> "${machinename}"
+	else	stdout "Test failed! look inside of '${machinename}' to reveal the failing numbers!" 
+		fatal  "Test failed! look inside of '${machinename}' to reveal the failing numbers!" >> "${machinename}"
 	fi
-	cat testing.bc >> "${machinename}"
-	[ -e log3 ] && cat log3 >> "${machinename}"
-	printf "\n\n" >> "${machinename}"
+	testcat log3 >> "${machinename}"
+	stdout >> "${machinename}"
+	stdout >> "${machinename}"
 	sleep 1 # we need 1 second in order to get a new random number
 done
 
