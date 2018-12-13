4146c73b0fbc7b3d74cf0327f9063e8f35f4c57b
graff <cm0graff@gmail.com>
2018-12-13 15:11:45 -0600

    basic operations are working

--- bc/bc.c
+++ bc/bc.c
@@ -1,8 +1,10 @@
 #include <arbitraire/arbitraire.h>
 #include <ctype.h>
+
+/* this is not a real implementation of bc, it is just a mock up to test arbitraire */
 int getword(char *word, size_t lim, FILE *fp)
 {
-	int c;
+	int c = 0;
 	char *w = word;
 	while (isspace(c = fgetc(fp)));
 		if (c != EOF)
@@ -28,7 +30,7 @@ int getword(char *word, size_t lim, FILE *fp)
 int main(int argc, char *argv[])
 {
 	FILE *o = fopen(argv[1], "r");
-	char c = 0;
+	
 	char *str = malloc(1000000);
 	size_t scale = 0;
 	char *string1 = NULL;
@@ -55,16 +57,48 @@ int main(int argc, char *argv[])
 			;
 		if (strcmp(str, "/") == 0)
 			division = 1;
+		
+		if (strcmp(str, "*") == 0)
+			multiplication = 1;
+		
+		if (strcmp(str, "+") == 0)
+			addition =1;
+		
+		if (strcmp(str, "-") == 0)
+			subtraction = 1;
+		
+		if (strcmp(str, "%") == 0)
+			modulus = 1;
 		if (isdigit(*str) && string1 == NULL)
 			string1 = strdup(str);
 		else if (isdigit(*str) && string2 == NULL)
 			string2 = strdup(str);
 		printf("%s\n", str);
 	}
+	fxdpnt *a = arb_str2fxdpnt(string1);
+	fxdpnt *b = arb_str2fxdpnt(string2);
+	fxdpnt *c = NULL;
+	
 	if (division)
-		printf("was division\n");
-	printf("scale was %zu\n", scale);
-	printf("string1 was %s\n", string1);
-	printf("string2 was %s\n", string2);	
+		c = arb_div(a, b, c, 10 , scale);
+	
+	if (multiplication)
+		c = arb_mul(a, b, c, 10 , scale);
+	
+	if (subtraction)
+		c = arb_sub(a, b, c, 10);
+	
+	if (addition)
+		c = arb_add(a, b, c, 10);
+	
+	if (modulus)
+		c = arb_mod(a, b, c, 10 , scale);
+	arb_print(c);
+	arb_free(a);
+	arb_free(b);
+	arb_free(c);
+	//printf("scale was %zu\n", scale);
+	//printf("string1 was %s\n", string1);
+	//printf("string2 was %s\n", string2);	
 	return 0;
 }
