32c43092c27adb51c364f2a911febdfb90695f2b
graff <cm0graff@gmail.com>
2018-12-13 14:59:05 -0600

    can now parse basic statements

--- bc/bc.c
+++ bc/bc.c
@@ -33,6 +33,11 @@ int main(int argc, char *argv[])
 	size_t scale = 0;
 	char *string1 = NULL;
 	char *string2 = NULL;
+	int division = 0;
+	int multiplication = 0;
+	int subtraction = 0;
+	int modulus = 0;
+	int addition = 0;
 	while (getword(str,1000000, o)!=EOF)
 	{
 		
@@ -48,13 +53,16 @@ int main(int argc, char *argv[])
 		}
 		if (strcmp(str, ";"))
 			;
-
+		if (strcmp(str, "/") == 0)
+			division = 1;
 		if (isdigit(*str) && string1 == NULL)
 			string1 = strdup(str);
 		else if (isdigit(*str) && string2 == NULL)
 			string2 = strdup(str);
 		printf("%s\n", str);
 	}
+	if (division)
+		printf("was division\n");
 	printf("scale was %zu\n", scale);
 	printf("string1 was %s\n", string1);
 	printf("string2 was %s\n", string2);	
