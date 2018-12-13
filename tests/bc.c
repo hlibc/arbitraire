b6de53fb20d0b37cccc18d82db80dc052f146b72
graff <cm0graff@gmail.com>
2018-12-13 14:30:03 -0600

    start a very basic parser for simple tests of large strings

---
+++ tests/bc.c
@@ -0,0 +1,39 @@
+#include <arbitraire/arbitraire.h>
+#include <ctype.h>
+int getword(char *word, size_t lim, FILE *fp)
+{
+	int c;
+	char *w = word;
+	while (isspace(c = fgetc(fp)));
+		if (c != EOF)
+			*w++ = c;
+	if (!isalnum(c) && !ispunct(c))
+	{
+		*w = '\0';
+		return c;
+	}
+	for ( ; --lim > 0; w++)
+	{
+		*w = fgetc(fp);
+		if (!isalnum(*w) && !ispunct(*w))
+		{
+			ungetc(*w, fp);
+			break;
+		}
+	}
+	*w = '\0';
+	return word[0];
+}
+
+int main(int argc, char *argv[])
+{
+	FILE *o = fopen(argv[1], "r");
+	char c = 0;
+	char *str = malloc(1000000);
+	while (getword(str,1000000, o)!=EOF)
+	{
+		printf("%s\n", str);
+	
+	}
+	return 0;
+}
