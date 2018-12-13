68ea2892e4c4f6656504beb2b84334a377b149ad
graff <cm0graff@gmail.com>
2018-12-13 14:53:14 -0600

    identify a few more things

--- tests/bc.c
+++ tests/bc.c
@@ -31,21 +31,32 @@ int main(int argc, char *argv[])
 	char c = 0;
 	char *str = malloc(1000000);
 	size_t scale = 0;
+	char *string1 = NULL;
+	char *string2 = NULL;
 	while (getword(str,1000000, o)!=EOF)
 	{
 		
 		if (strcmp(str, "scale") == 0)
 		{
-			//scale = strtol(str, NULL, 10);
 			getword(str,1000000, o);
 			if (strcmp(str, "=") == 0)
 			{
 				getword(str,1000000, o);
 				scale = strtol(str, NULL, 10);
 			}
+			getword(str,1000000, o);
 		}
+		if (strcmp(str, ";"))
+			getword(str,1000000, o);
+
+		if (isdigit(*str) && string1 == NULL)
+			string1 = strdup(str);
+		else if (isdigit(*str) && string2 == NULL)
+			string2 = strdup(str);
 		printf("%s\n", str);
 	}
 	printf("scale was %zu\n", scale);
+	printf("string1 was %s\n", string1);
+	printf("string2 was %s\n", string2);	
 	return 0;
 }
