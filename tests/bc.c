30390a71f2e86fba4f066d84595513424e5d8a88
graff <cm0graff@gmail.com>
2018-12-13 14:37:10 -0600

    continue work with the basic parser

--- tests/bc.c
+++ tests/bc.c
@@ -30,10 +30,15 @@ int main(int argc, char *argv[])
 	FILE *o = fopen(argv[1], "r");
 	char c = 0;
 	char *str = malloc(1000000);
+	size_t scale = 0;
 	while (getword(str,1000000, o)!=EOF)
 	{
+		
+		if (strcmp(str, "scale") == 0)
+		{
+			scale = strtol(str, NULL, 10);
+		}
 		printf("%s\n", str);
-	
 	}
 	return 0;
 }
