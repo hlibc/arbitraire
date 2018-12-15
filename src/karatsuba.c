6a410fcdd6d72fbf1f9beb7f855309fafa3feaea
graff <cm0graff@gmail.com>
2018-12-15 16:21:37 -0600

    karatsuba is passing more tests

--- src/karatsuba.c
+++ src/karatsuba.c
@@ -5,51 +5,6 @@
         12345678 * 55559999
 
         1234*5555*10^8 + (1234*9999 + 5678*5555)*10^4 + 5678*9999
-
-	for an odd split
-
-	1235678 * 5559999
-	123*555*10^8 + (123*9999 + 5678*555)*10^4 + 5678*9999
-
-
-	for an uneven split
-
-	1235678 * 55559999
-	123*5555*10^8 + (123*9999 + 5678*5555)*10^4 + 5678*9999
-
-	a dimishing uneven split requres smaller powers
-	12345 * 55559999
-
-	12*5555*10^7 + (12*9999 + 345*5555)*10^4 + 345*9999 
-
-
-	notes:
-
-	instead of copying the needed data it would be faster to expand the
-	magnitudes and then make the new partial number's ->number be
-	pointers to sections of the originals. -- the problem with this is
-	that we would need to adjust ->allocated, ->len and ->lp accordingly
-
-
-	As a trivial speedup we could combine the steps that create variable
-	copies of the multiplicands for long-multiplication ie;
-		fxdpnt *a2 = arb_expand(NULL, MAX(scale, a->len));
-	and then some of the time complexity could be hidden behind this rather
-	important/useful step. This is ofc just an ease of use step and 
-	ultimately much faster methods must be divised to split numbers and for
-	long multiplication to handle scale. Though, in sub-add we use the
-	arb_place mechanism to feed an imaginary array of zeros -- this comes at
-	a computational cost of added conditional tests.
-
-	It may be best to view number expansion as a fast way of avoiding these
-	kinds of conditional methods of supplyng imaginary arrays of zeros to
-	represent the trailing end of a number. In order to determine the best way
-	we'll need tests of both types of functions.
-
-	IMPORTANT:
-
-		this karatsuba algorithm does not work unless supplied a mul
-	algorithm which does *not* strip leading zeros
 */
 
 size_t split(fxdpnt *a, fxdpnt *b, fxdpnt **aa, fxdpnt **bb, fxdpnt **cc, fxdpnt **dd)
@@ -142,13 +97,6 @@ fxdpnt *karatsuba2(fxdpnt *a, fxdpnt *b, fxdpnt *c, int base, size_t scale)
 	mid2->len = mid2->lp = ((aa->len + bb->len + aa->len));
 	/* sum middle halves */
 	add(mid1, mid2, &midtot, base, "midtot = ");
-	//if (iszero(mid2)) {
-	//	arb_expand(mid1, mid1->len + 1);
-	//	mid1->len++;
-	//}
-	/* expand to the power of */
-	//arb_expand(midtot, (aa->len + bb->len + aa->len));
-	//midtot->len = midtot->lp = ((aa->len + bb->len + aa->len));
 	/* sum into total */
 	add(midtot, front, &total, base, "total = ");
 	/* end halves */
@@ -158,61 +106,6 @@ fxdpnt *karatsuba2(fxdpnt *a, fxdpnt *b, fxdpnt *c, int base, size_t scale)
 
 	total->lp = (total->len - comp);
 	//total->lp = total->lp - (total->lp - lpstore);
-	return total;
-}
-
-fxdpnt *karatsuba(fxdpnt *a, fxdpnt *b, fxdpnt *c, int base, size_t scale)
-{
-	c = c;
-	scale = scale;
-	/* divide the number into halves */
-	size_t ha = a->len / 2;
-	size_t hb = b->len / 2;
-	fxdpnt *front = arb_expand(NULL, ha + hb);
-	fxdpnt *mid1 = arb_expand(NULL, ha + hb);
-	fxdpnt *mid2 = arb_expand(NULL, ha + hb);
-	fxdpnt *midtot = arb_expand(NULL,a->len + b->len);
-	fxdpnt *end = arb_expand(NULL, ha + hb);
-	fxdpnt *total = arb_expand(NULL, a->len + b->len);
-
-
-
-	/* front half */
-	front->lp=front->len= arb_mul_core(a->number, ha, b->number, hb, front->number, base);
-
-	/* expand to the power of */
-	arb_expand(front, a->len + b->len);
-	front->lp = front->len = a->len + b->len;
-	arb_print(front);
-	/* sum into total */
-	add(front, total, &total, base, 0);
-
-
-	/* middle halves */
-	mid1->lp=mid1->len= arb_mul_core(a->number, ha, b->number + hb, hb, mid1->number, base);
-	arb_print(mid1);
-	mid2->lp=mid2->len= arb_mul_core(a->number + ha, ha, b->number, hb, mid2->number, base);
-	arb_print(mid2);
-
-	/* sum middle halves */
-	add(mid1, mid2, &midtot, base, 0);
-	midtot->lp = midtot->len;
-
-	/* expand to the power of */
-	arb_expand(midtot, (ha + hb) * 2);
-	midtot->lp = midtot->len = (ha + hb) + ((ha + hb)/2);
-	/* sum into total */
-	add(midtot, total, &total, base, 0);
-
-	/* end halves */
-	end->lp=end->len= arb_mul_core(a->number + ha, ha, b->number + hb, hb, end->number, base);
-	arb_print(end);
-	/* sum into total */
-	add(end, total, &total, base, 0);
-	
-
-	
-
 	return total;
 }
 
