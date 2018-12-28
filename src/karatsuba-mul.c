b252cb91dcafca2cc76a8aae543c7dc18a33dcc3
graff <cm0graff@gmail.com>
2018-12-28 10:28:29 -0600

    use temporary pointers to the number sections instead of data copying. the algorithm still appears very slow so there may be an error in the math accumulating naive values

--- src/karatsuba-mul.c
+++ src/karatsuba-mul.c
@@ -1,50 +1,49 @@
 #include "internal.h"
+
 static fxdpnt *arb_karatsuba_mul_core(fxdpnt *x, fxdpnt *y, fxdpnt *z, int base)
 {
 	size_t size = 0;
-	if (y->len == 1) {
-		mul2(x, y, &z, base, size, 0);
+	if (y->len < 400) {
+		z = arb_mul2(x, y, z, base, size);
 		return z;
-	} else if (x->len == 1) {
-		mul2(y, x, &z, base, size, 0);
+	} else if (x->len <400) {
+		z = arb_mul2(y, x, z, base, size);
 		return z;
 	}
 
-	size_t m = ((MIN(x->len, y->len)) / 2);
+	size_t m = ((MIN(x->len, y->len)+1) / 2);
 
-	fxdpnt *x1 = arb_expand(NULL, x->len - m + size);
-	fxdpnt *y1 = arb_expand(NULL, y->len - m + size);
-	fxdpnt *x0 = arb_expand(NULL, m	+size);
-	fxdpnt *y0 = arb_expand(NULL, m + size);
+	fxdpnt x1[1] = { 0 };
+	fxdpnt y1[1] = { 0 };
+	fxdpnt x0[1] = { 0 };
+	fxdpnt y0[1] = { 0 };
 
-
-	_arb_copy_core(x1->number, x->number, (x->len - m));
-	_arb_copy_core(y1->number, y->number, (y->len - m));
-	_arb_copy_core(x0->number, x->number + x->len - m, m);
-	_arb_copy_core(y0->number, y->number + y->len - m, m);
-
+	x1->number = x->number;
 	x1->lp = x1->len = x->len - m;
+	y1->number = y->number;
 	y1->lp = y1->len = y->len - m;
+	x0->number = x->number + x->len - m;
 	x0->lp = x0->len = m;
+	y0->number = y->number + y->len - m;
 	y0->lp = y0->len = m;
 
-	fxdpnt *z1 = arb_expand(NULL, size);
-	fxdpnt *z2 = arb_expand(NULL, size);
-	fxdpnt *z3 = arb_expand(NULL, size);
-	fxdpnt *z4 = arb_expand(NULL, size);
+	/* these variables all get their memory from the calling functions */
+	fxdpnt *z1 = NULL;
+	fxdpnt *z2 = NULL;
+	fxdpnt *z3 = NULL;
+	fxdpnt *z4 = NULL;
+	fxdpnt *z6 = NULL;
+	fxdpnt *z7 = NULL;
 	fxdpnt *z5 = arb_expand(NULL, size);
 
-
 	z1 = arb_karatsuba_mul_core(x1, y1, z1, base);
-
+	z4 = arb_karatsuba_mul_core(x0, y0, z4, base);
+	
 	z2 = arb_add2(x1, x0, z2, base);
 	z3 = arb_add2(y1, y0, z3, base);
-	z4 = arb_karatsuba_mul_core(x0, y0, z4, base);
 	z5 = arb_karatsuba_mul_core(z2, z3, z5, base);
 
 
-	fxdpnt *z6 = arb_expand(NULL, size);
-	fxdpnt *z7 = arb_expand(NULL, size);
 	z6 = arb_sub(z5, z1, z6, base);
 	z7 = arb_sub(z6, z4, z7, base);
 	z7 = arb_expand(z7, z7->len + m);
@@ -55,14 +54,10 @@ static fxdpnt *arb_karatsuba_mul_core(fxdpnt *x, fxdpnt *y, fxdpnt *z, int base)
 	z1->lp += 2 * m;
 	z1->len += 2 * m;
 
-	fxdpnt *z8 = arb_expand(NULL, size);
+	fxdpnt *z8 = NULL;
 	z8 = arb_add2(z1, z7, z8, base);
 	z = arb_add2(z8, z4, z, base);
 
-	arb_free(x0);
-	arb_free(y0);
-	arb_free(x1);
-	arb_free(y1);
 	arb_free(z1);
 	arb_free(z2);
 	arb_free(z3);
@@ -87,7 +82,6 @@ fxdpnt *arb_karatsuba_mul(fxdpnt *a, fxdpnt *b, fxdpnt *c, int base, size_t scal
 	c2->lp = a2->lp + b2->lp;
 	c2->len = MIN(rr(a2) + rr(b2), MAX(scale, MAX(rr(a2), rr(b2)))) + c2->lp;
 	c2 = remove_leading_zeros(c2);
-	 //c2->len--;
 	arb_free(a2);
 	arb_free(b2);
 	if (c)
