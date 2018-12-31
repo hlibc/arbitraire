bcb73c80f63329551fa382983fc105c172a368d7
graff <cm0graff@gmail.com>
2018-12-31 11:41:01 -0600

    commit sin

---
+++ src/sin.c
@@ -0,0 +1,60 @@
+#include "internal.h"
+
+fxdpnt arb_sin(fxdpnt *x, int base, size_t scale)
+{
+/*
+        int i;
+        int j;
+        int sign = 1;
+        double d;
+        double z = 0;
+        double y = 0;
+
+        while ( x >= (2*PI))
+                x -= (2*PI);
+
+        while ( x < 0 )
+                x += (2*PI);
+*/
+
+
+	fxdpnt *i = arb_str2fxdpnt("0");
+	fxdpnt *j = arb_str2fxdpnt("0");
+	fxdpnt *d = arb_str2fxdpnt("1");
+	fxdpnt *t = arb_str2fxdpnt("0");
+	int c = 0;
+
+	for ( ; 1 ; )
+	{
+		j = arb_mul(two, i, j, base, scale);
+		j = arb_add(j, one, j, base);
+		
+		for (;1;)
+		{
+			if ((c = arb_compare(j, zero, base) != 1))
+				break;
+				
+			t = arb_div(x, j, t, base, scale);
+			d = arb_mul(d, t, d, base, scale);
+			decr(&j, base, 0);
+		}
+		incr(&i, base, 0);
+	}
+/*
+        for (i = 0; 1; i++)
+        {
+                d = 1.0;
+                for (j = (2*i) + 1 ; j > 0; j--)
+                {
+                        d *= (x / j);
+                }
+                y += d * sign;
+                sign = -sign;
+                if ( y == z )
+                        break;
+                z = y;
+        }
+
+        return y;
+*/
+}
