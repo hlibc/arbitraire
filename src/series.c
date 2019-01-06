b333f4e30692287a7b862cadb9baacb36f76383a
graff <cm0graff@gmail.com>
2019-01-06 06:52:12 -0600

    have arb_exp use the same style and naming conventions as the other series

--- src/series.c
+++ src/series.c
@@ -52,36 +52,34 @@ fxdpnt *arb_series(fxdpnt *x, int base, size_t scale, int needsone, int hyperbol
 
 fxdpnt *arb_exp_trans(fxdpnt *x, int base, size_t scale)
 {
-        fxdpnt *i = arb_str2fxdpnt("0");
-        fxdpnt *n = arb_str2fxdpnt("2");
+        fxdpnt *j = arb_str2fxdpnt("0");
+        fxdpnt *i = arb_str2fxdpnt("2");
         fxdpnt *y = arb_str2fxdpnt("0");
         fxdpnt *d = arb_str2fxdpnt("0");
         fxdpnt *z = arb_str2fxdpnt("0");
         fxdpnt *t = arb_str2fxdpnt("0");
 
         add(one, x, &y, base, 0);
-        int c = 0;
+
         do {
                 d = arb_copy(d, x);
-                i = arb_copy(i, two);
-                while (1) {
-                        c = arb_compare(i, n, base);
-                        if (c == 1)
-                                break;
-
-                        divv(x, i, &t, base, scale, 0);
+                j = arb_copy(j, two);
+                do {
+                        if (arb_compare(j, i, base) == 1)
+				break;
+                        divv(x, j, &t, base, scale, 0);
                         mul(d, t, &d, base, scale, 0);
-                        incr(&i, base, 0);
-                }
+                        incr(&j, base, 0);
+                } while(1);
                 add(y, d, &y, base, 0);
-                incr(&n, base, 0);
-                if ((c = arb_compare(y, z, base)) == 0)
+                incr(&i, base, 0);
+                if (arb_compare(y, z, base) == 0)
                         break;
                 z = arb_copy(z, y);
-        }while (1);
+        } while (1);
 
+	arb_free(j);
 	arb_free(i);
-	arb_free(n);
 	arb_free(d);
 	arb_free(z);
 	arb_free(t);
