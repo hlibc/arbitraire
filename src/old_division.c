6a1245006b8de8fc0331f4dd7bf51fcb02795fb1
graff <cm0graff@gmail.com>
2019-01-18 20:22:53 -0600

    place the radix

--- src/old_division.c
+++ src/old_division.c
@@ -18,8 +18,27 @@ fxdpnt *old_div(fxdpnt *num, fxdpnt *den, fxdpnt *q, int b, size_t scale)
 	
 	_arb_memset(u, 0, width);
 	_arb_copy_core(u, num->number, num->len);
-	_arb_memset(q->number, 0, num->len);
+	arb_expand(q, num->len + scale);
+	_arb_memset(q->number, 0, num->len + scale);
 	q->number[z] = 0;
+
+	/* compute the scales for the final solution */
+        size_t lea = rl(num) + rr(den);
+	size_t leb = den->len;
+        q->lp = 1;
+        if (leb > lea+scale) {
+                q->len = q->lp + scale;
+                goto end;
+        } else {
+                if (!(leb>lea))
+                        q->lp = lea - leb + 1;
+        }
+        q->len = q->lp + scale;
+        /* begin the division operation */
+
+        if (leb > lea)
+                j = (leb-lea);
+
 
 	for (; z < num->len;)
 	{ 
@@ -51,6 +70,7 @@ fxdpnt *old_div(fxdpnt *num, fxdpnt *den, fxdpnt *q, int b, size_t scale)
 			q->number[z] += 1;
 		}
 	} 
+	end:
 	free(u);
 	free(v);
 	return q;
