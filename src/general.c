d2c87a9cb71cad3aa153a0c81fd5196da5e92fc8
hlibc <cm0graff@gmail.com>
2019-03-03 04:48:37 -0500

    move print-attrs

--- src/general.c
+++ src/general.c
@@ -158,3 +158,12 @@ size_t arb_left(fxdpnt *a)
 	return a->lp;
 }
 
+#include "internal.h"
+
+void arb_attrs(fxdpnt *a, char *m)
+{
+	printf("%s->len = %zu\n", m, arb_size(a));
+        printf("%s->lp = %zu\n", m, arb_left(a));
+        printf("%s->allocated = %zu\n", m, arb_allocated(a));
+        printf("%s->sign = %c\n", m, arb_sign(a));
+}
