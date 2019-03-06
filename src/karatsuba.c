0a87aa0a01c7d9232e4d4de4bb3c357738387815
hlibc <cm0graff@gmail.com>
2019-03-06 07:50:03 -0500

    reduce the tolerance alloted to karatsuba multiplication

--- src/karatsuba.c
+++ src/karatsuba.c
@@ -3,7 +3,8 @@
 /*
 	Karatsuba multiplication
 
-	arb_add2 and arb_sub2 are simply variants which do not strip zeros.
+	arb_add2 and arb_sub2 are simply variants of arb_add and arb_sub which 
+	do not strip zeros.
 
 	Copyright Bao Hexing and CM Graff 2018-2019
 */
@@ -64,8 +65,8 @@ static fxdpnt *karatsuba(const fxdpnt *a, const fxdpnt *b, fxdpnt *c, int base)
 
 fxdpnt *arb_karatsuba_mul(const fxdpnt *a, const fxdpnt *b, fxdpnt *c, int base, size_t scale)
 { 
-	/* karatsuba is described as taking an extra bit/limb, hence the +3 */
-	fxdpnt *c2 = arb_expand(NULL, a->len + b->len + 3);
+	/* karatsuba is described as taking an extra bit/limb, hence the +1 */
+	fxdpnt *c2 = arb_expand(NULL, a->len + b->len + 1);
 	c2 = karatsuba(a, b, c2, base);
 	arb_setsign(a, b, c2);
 	c2->lp = a->lp + b->lp;
