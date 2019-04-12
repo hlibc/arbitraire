94e147f6bfc3a98cde7098ec374ebf803a8322b9
hlibc <cm0graff@gmail.com>
2019-04-11 23:49:16 -0400

    only count fractional zeros

--- src/count_leading_fractional_zeros.c
+++ src/count_leading_fractional_zeros.c
@@ -4,11 +4,13 @@
 size_t count_leading_fractional_zeros(fxdpnt *a)
 {
 	size_t pos = a->len - (a->len - a->lp);
+	size_t ret = 0;
 	while (a->number[pos] == 0)
 	{
 		pos++;
+		ret++;
 	}
-	return pos;
+	return ret;
 }
 
 
