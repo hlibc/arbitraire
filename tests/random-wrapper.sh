884eaaff213f6a7e8664cd290acb38b7d16587a1
hlibc <cm0graff@gmail.com>
2019-05-04 16:26:23 -0400

    the test shell script now reports the time used for both the control and the test

--- tests/random-wrapper.sh
+++ tests/random-wrapper.sh
@@ -5,7 +5,7 @@ set -e
 . ./.config
 
 if [ "$#" -lt 3 ]
-then	fatal "Usage: ./tests/random-wrapper.sh div|sub maxima agnostic|null"
+then	fatal "Usage: ./tests/random-wrapper.sh div|sub maxima agnostic|null strace|valgrind|time [NUMBER OF TESTS]"
 fi
 
 USE_VALGRIND="0"
@@ -17,14 +17,12 @@ then	[ "$4" = "valgrind" ] && USE_VALGRIND="1"
 	[ "$4" = "time" ] && USE_TIME="1"
 fi
 
-COUNT="0"
-machinename="$(gcc -dumpmachine)"
+machinename="$(uname -m)"
 machinename="${machinename}-tests-passed.txt"
 
-TOTAL=100
-
+NUMTESTS="100"
 if [ $# = "5" ]
-then	TOTAL="$5"
+then	NUMTESTS="$5"
 fi
 
 stdout "This is a set of PRNG tests to ensure that arbitraire works" >>"${machinename}"
@@ -37,27 +35,32 @@ stdout "The tests should halt upon detecting an error and the contents" >>"${mac
 stdout "of 'testing.bc' can be inspected to reveal the failing test" >>"${machinename}"
 stdout >> "${machinename}"
 stdout >> "${machinename}"
-while [ "$COUNT" -lt "$TOTAL" ]
-do	COUNT="$((COUNT + 1))"
+while [ "$NUMTESTS" -gt "0" ]
+do	NUMTESTS="$((NUMTESTS - 1))"
 	stdout "Test number: ${COUNT}" >>"${machinename}"
 	if [ "$USE_VALGRIND" = "1" ]
 	then	valgrind --leak-check=full ./tests/random-tests "$1" "$2" "$3" >log 2>log3
+		bc -l testing.bc > log2 2>log4
 	elif [ "$USE_STRACE" = "1" ]
 	then	strace ./tests/random-tests "$1" "$2" "$3" >log 2>log3
+		bc -l testing.bc > log2 2>log4
 	elif [ "$USE_TIME" = "1" ]
 	then	time ./tests/random-tests "$1" "$2" "$3" >log 2>log3
+		time bc -l testing.bc > log2 2>log4
 	else	./tests/random-tests "$1" "$2" "$3" >log 2>log3
+		bc -l testing.bc > log2 2>log4
 	fi
-	
-	bc -l testing.bc > log2
-	testcat testing.bc >> "${machinename}"
+	truecat testing.bc >> "${machinename}"
 	if diff log log2
-	then	stdout "Giant pseudo-random number test passed."
-		stdout "Giant pseudo-random number test passed." >> "${machinename}"
+	then	stdout "PRNG driven test passed."
+		stdout "PRNG driven number test passed." >> "${machinename}"
 	else	stdout "Test failed! look inside of '${machinename}' to reveal the failing numbers!" 
 		fatal  "Test failed! look inside of '${machinename}' to reveal the failing numbers!" >> "${machinename}"
 	fi
-	testcat log3 >> "${machinename}"
+	stdout "stderr log of the test subject" >> "${machinename}"
+	truecat log3 >> "${machinename}"
+	stdout "stderr log of the control" >> "${machinename}"
+	truecat log4 >> "${machinename}"
 	stdout >> "${machinename}"
 	stdout >> "${machinename}"
 	sleep 1 # we need 1 second in order to get a new random number
