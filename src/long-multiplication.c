4714edb3aa539a8e545483f7e6975d0e7902f782
hlibc <cm0graff@gmail.com>
2019-05-07 04:14:15 -0400

    comba passes 6 out of 7 PRNG tests. the error indicates an incorrect carry

--- src/long-multiplication.c
+++ src/long-multiplication.c
@@ -35,7 +35,13 @@
 	by moving the carrying to a final step.
 */
 
-
+static void _pr(const UARBT *a, size_t len)
+{
+	size_t i = 0;
+	for (i=0;i<len;++i)
+		fputc(arb_highbase(a[i]), stdout);
+	fputc('\n', stdout);
+}
 size_t arb_mul_comba_core(const UARBT *a, size_t alen, const UARBT *b, size_t blen, UARBT *c, int base)
 {
 	/* Comba multiplication. This algorithm is still under construction */
@@ -51,7 +57,8 @@ size_t arb_mul_comba_core(const UARBT *a, size_t alen, const UARBT *b, size_t bl
 		summated and then the carries performed (fast comba).
 
 		Fast comba can only be used for smaller inputs or it will lead to
-		type overflow.
+		type overflow. This would be referred to as the acyclic or linear 
+		convolution 
 
 		Ideally, comba rows would stary at index 1 so that the carries can
 		be performed without reallocation or copying memory. This would have
@@ -71,7 +78,7 @@ size_t arb_mul_comba_core(const UARBT *a, size_t alen, const UARBT *b, size_t bl
 
 	/* number of rows and the length of rows are the same -- plus extra zeros */
 	size_t numrows = MAX(alen, blen);
-	size_t rowlen = numrows + numrows;
+	size_t rowlen = alen + blen + 1;
 
 	UARBT **rows = arb_malloc((numrows * 10) * sizeof(UARBT));
 	memset(c, 0, alen +blen);
@@ -79,38 +86,42 @@ size_t arb_mul_comba_core(const UARBT *a, size_t alen, const UARBT *b, size_t bl
 	size_t rowc = 0; 
 	size_t last = 0;
 	for (i = alen; i > 0 ; i--) {
-		rows[rowc] = arb_calloc(1,( rowlen * 10) * sizeof(UARBT) ); 
+		rows[rowc] = arb_calloc(1,( rowlen * 100) * sizeof(UARBT) ); 
 		for (j = blen, k = i + j; j > 0 ; j--, k--){ 
 			rows[rowc][k-1] = a[i-1] * b[j-1];
 		} 
 		++rowc; 
 	}
-	fprintf(stderr, "addition stage\n");
+
 
 	size_t z = 0;
-	size_t endlen = rowlen;
+	size_t endlen = numrows;
 	/* pass over all rows */
 	for(;z<rowc;++z) {
 		
 		/* pass over all digits in a row */
 		for (j=rowlen;j>0; j--){
-			carry = 0;
 			carry = rows[z][j] / base;
-			prod = rows[z][j];
+			rows[z][j] = rows[z][j] % base;
 			rows[z][j - 1] += carry;
-			rows[z][j] = prod % base;
-		}
-		/* this carry is probbaly not correct, as this is a mul carry */
-		/*
-		if (carry) { 
+			
+			//printf("\n");
+	                //for(i = 0;i<rowlen;++i) {
+        	        //        printf("%u ", rows[z][i]);
+	                //}
+	                //printf("\n");
+		} 
+		if ( j == 0 && carry)
+		{
+
 			for (i = rowlen + 1; i > 0; i--) {
 				rows[z][i] = rows[z][i-1];
 			}
 			rows[z][0] = 1;
+			//endlen += 1;
 		}
-		*/
 		/* summate the current row into the total 'c' */
-		for (carry=0, i=rowlen; i>0;--i) { 
+		for (carry=0, i=rowlen; i>0;--i,--j) {
 			sum = c[i] + rows[z][i] + carry;
 			carry = 0;
 			if(sum >= base) {
@@ -120,18 +131,29 @@ size_t arb_mul_comba_core(const UARBT *a, size_t alen, const UARBT *b, size_t bl
 			c[i] = sum;
 		}
 		if (carry) {
-			for (i = rowlen + 1; i > 0; i--) {
+			for (i = endlen + 1; i > 0; i--) {
 				c[i] = c[i-1];
 			}
 			c[0] = 1;
 			endlen += 1;
 		}
-	} 
+		//printf("\ntotal\n");
+                /* now print the row  and see if we got it right */
+                //for(i = 0;i<endlen;++i) {
+                  //      printf("%u ", c[i]);
+                //}
+                /* now sum the row into a running total */
+                //printf("\n");
+
+	}
 	return ret;
 }
 
 size_t arb_mul_core(const UARBT *a, size_t alen, const UARBT *b, size_t blen, UARBT *c, int base)
 {
+	/* TODO: a memset is probably faster than the embedded conditional
+	 * so the assembly code needs to be examined and some timing tests performed
+	 */
 	UARBT prod = 0;
 	UARBT carry = 0;
 	size_t i = 0;
@@ -210,7 +232,7 @@ fxdpnt *arb_mul(const fxdpnt *a, const fxdpnt *b, fxdpnt *c, int base, size_t sc
 
 fxdpnt *arb_comba(const fxdpnt *a, const fxdpnt *b, fxdpnt *c, int base, size_t scale)
 {
-	fxdpnt *c2 = arb_expand(NULL, a->len + b->len);
+	fxdpnt *c2 = arb_expand(NULL,( a->len + b->len) * 10);
 	arb_setsign(a, b, c2);
         arb_mul_comba_core(a->number, a->len, b->number, b->len, c2->number, base);
         c2->lp = rl(a) + rl(b);
